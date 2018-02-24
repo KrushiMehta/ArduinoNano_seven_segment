11caea703d99e3d332eae3fb9c19978a7558e103
Krushi <krushi27.km@gmail.com>
2018-02-24 12:19:44 +0530

    Update seven_segment.ino

--- seven_segment.ino
+++ seven_segment.ino
@@ -1,7 +1,7 @@
 void setup() {
-  // put your setup code here, to run once:
+
    int i=0;
-   for(i=2; i<=9; i++)
+   for(i=2; i<=9; i++) // make all 2 to 9 digital pin on high
    {
     pinMode(i, OUTPUT);
     }
@@ -9,8 +9,19 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
- 
+  // to display a digit from 0 to 9 change it to low
+  
+   digitalWrite(2, LOW);
+   digitalWrite(3, LOW);
+   digitalWrite(4, LOW);
+   digitalWrite(5, LOW);
+   digitalWrite(6, LOW);
+   digitalWrite(7, LOW);
+   digitalWrite(8, HIGH);
+   digitalWrite(9, LOW);
+   delay(2000); 
+// display 0 
+  
    digitalWrite(2, HIGH);
    digitalWrite(3, LOW);
    digitalWrite(4, LOW);
@@ -20,7 +31,7 @@ void loop() {
    digitalWrite(8, HIGH);
    digitalWrite(9, LOW);
    delay(2000);
-//display 1 numeric
+//display 1 
 
    digitalWrite(2, LOW);
    digitalWrite(3, LOW);
@@ -31,7 +42,7 @@ void loop() {
    digitalWrite(8, LOW);
    digitalWrite(9, LOW);
    delay(2000);
-// display 2
+// display 2 
 
    digitalWrite(2, LOW);
    digitalWrite(3, LOW);
@@ -110,14 +121,4 @@ void loop() {
    delay(2000); 
    // display 9
 
-   digitalWrite(2, LOW);
-   digitalWrite(3, LOW);
-   digitalWrite(4, LOW);
-   digitalWrite(5, LOW);
-   digitalWrite(6, LOW);
-   digitalWrite(7, LOW);
-   digitalWrite(8, HIGH);
-   digitalWrite(9, LOW);
-   delay(2000); 
-   // display 0
 }

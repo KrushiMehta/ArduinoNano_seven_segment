e454f0b361061f6e2fc7c32c39fe73e79d30bea4
Krushi <krushi27.km@gmail.com>
2018-03-27 16:07:04 +0530

    Update seven_segment.ino

--- seven_segment.ino
+++ seven_segment.ino
@@ -55,36 +55,36 @@ void loop() {
    delay(2000); 
    // display 3
 
-   digitalWrite(2, HIGH); //a
-   digitalWrite(3, LOW); //b
-   digitalWrite(4, LOW); //c
-   digitalWrite(5, HIGH); //d
-   digitalWrite(6, HIGH); //e
-   digitalWrite(7, LOW); //f
-   digitalWrite(8, LOW); //g
-   digitalWrite(9, LOW); //dot
+   digitalWrite(2, HIGH); 
+   digitalWrite(3, LOW); 
+   digitalWrite(4, LOW); 
+   digitalWrite(5, HIGH); 
+   digitalWrite(6, HIGH);
+   digitalWrite(7, LOW); 
+   digitalWrite(8, LOW); 
+   digitalWrite(9, LOW); 
    delay(2000); 
    // display 4
    
-   digitalWrite(2, LOW); //a
-   digitalWrite(3, HIGH); //
-   digitalWrite(4, LOW); //c
-   digitalWrite(5, LOW); //d
-   digitalWrite(6, HIGH); //
-   digitalWrite(7, LOW); // f
-   digitalWrite(8, LOW); //g
-   digitalWrite(9, LOW); //dot
+   digitalWrite(2, LOW); 
+   digitalWrite(3, HIGH); 
+   digitalWrite(4, LOW); 
+   digitalWrite(5, LOW); 
+   digitalWrite(6, HIGH);
+   digitalWrite(7, LOW); 
+   digitalWrite(8, LOW); 
+   digitalWrite(9, LOW); 
    delay(2000); 
    // display 5
 
-   digitalWrite(2, LOW); // a
-   digitalWrite(3, HIGH); //b
-   digitalWrite(4, LOW); //c
-   digitalWrite(5, LOW); //d
-   digitalWrite(6, LOW); //e
-   digitalWrite(7, LOW); //f
-   digitalWrite(8, LOW); //g
-   digitalWrite(9, LOW); //dot
+   digitalWrite(2, LOW); 
+   digitalWrite(3, HIGH); 
+   digitalWrite(4, LOW); 
+   digitalWrite(5, LOW); 
+   digitalWrite(6, LOW); 
+   digitalWrite(7, LOW); 
+   digitalWrite(8, LOW); 
+   digitalWrite(9, LOW); 
    delay(2000); 
    //display 6
 
